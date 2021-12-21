7334ac11eb21bb93306953cf3fdcc8fe6b41bb28
Tran-Anh-Duc <trananhducty@gmail.com>
2021-12-21 09:40:45 +0700

    them

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  user =
+    {
+      nameCard:"tran anh duc",
+      email:"trananhducty@gmail.com",
+      phone:'0362272729'
+    }
+
+
   title = 'angular-name-card';
+
 }
